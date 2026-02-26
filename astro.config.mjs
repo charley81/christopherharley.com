8261ef14464e6bf2608f7b0648d0dc30cc37e695
ChrisKnowsDev <chrisharley81@gmail.com>
2026-02-25 23:21:10 -0500

    add netlify adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig, fontProviders } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'static',
+
   experimental: {
     fonts: [
       {
@@ -16,7 +19,10 @@ export default defineConfig({
       },
     ],
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 })
